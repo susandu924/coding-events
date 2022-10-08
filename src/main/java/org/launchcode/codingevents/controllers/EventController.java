421ca8296d74aafa8dd4a077748b8825d4d6e403
Susan <susandu924@gmail.com>
2022-10-08 15:46:35 -0500

    coding-events-my-views-exercise-solutions branch cute!!!

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,30 +8,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-
-private static List<String>events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+        /*List<String>events = new ArrayList<>();
+        events.add("Code With Pride");
+        events.add("Strange Loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
+
+        model.addAttribute("events", events);*/
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Hour of Code", "Getting young minds interested in coding.");
+        events.put("Strange Loop", "Coding conference of professionals sharing ideas.");
+        events.put("CES", "Annual trade show showing awesome new tech.");
         model.addAttribute("events", events);
         return "events/index";
     }
 
 //    lives at /events/create
-    @GetMapping("create")
-    public String renderCreateEventsForm(){
-        return "events/create";
-    }
-    @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
-        return "redirect:";
-//        redirects to the displayAllEvents
-    }
+//    @GetMapping("create")
+//    public String renderCreateEventsForm(){
+//        return "events/create";
+//    }
+//    @PostMapping("create")
+//    public String createEvent(@RequestParam String eventName){
+//        events.add(eventName);
+//        return "redirect:";
+////        redirects to the displayAllEvents
+//    }
 }
