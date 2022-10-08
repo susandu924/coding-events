e5b7bd5740ad5ec4899ec4da2da8fa88c1b6a50f
Susan <susandu924@gmail.com>
2022-10-08 18:25:13 -0500

    coding-events-create-a-model branch 14.3 create a data layer

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +16,11 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-private static List<Event>events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
+//        calls static method off object
         return "events/index";
     }
 
@@ -32,7 +32,7 @@ private static List<Event>events = new ArrayList<>();
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName,
                               @RequestParam String eventDescription){
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
 //        redirects to the displayAllEvents
     }
